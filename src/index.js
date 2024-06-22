0efc440badc4a4264b79d873304a8aa8b1dc36b9
cccyn <a1406270347@gmail.com>
2024-06-22 18:50:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.cccyn.win": dockerHub,
+  "quay.cccyn.win": "https://quay.io",
+  "gcr.cccyn.win": "https://gcr.io",
+  "k8s-gcr.cccyn.win": "https://k8s.gcr.io",
+  "k8s.cccyn.win": "https://registry.k8s.io",
+  "ghcr.cccyn.win": "https://ghcr.io",
+  "cloudsmith.cccyn.win": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cccyn.win": dockerHub,
 };
 
 function routeByHosts(host) {
